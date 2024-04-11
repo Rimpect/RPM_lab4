b167a48600646ea4fed66d9d3ac54cc92b4f27a7
maks <gitlaborhub@gmail.com>
2024-04-11 12:30:21 +0300

    Initial commit first

---
+++ app/src/main/java/com/example/laba4/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.laba4;
+
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String FILENAME = "sample.txt"; // имя файла
+    private EditText mEditText;
+
+    Toolbar myToolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_open:
+                openFile(FILENAME);
+                return true;
+            case R.id.action_save:
+                saveFile(FILENAME);
+                return true;
+            default:
+                return true;
+        }
+    }
+
+    // Метод для открытия файла
+    private void openFile(String fileName) {
+        try {
+            InputStream inputStream = openFileInput(fileName);
+
+            if (inputStream != null) {
+                InputStreamReader isr = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(isr);
+                String line;
+                StringBuilder builder = new StringBuilder();
+
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line + "\n");
+                }
+
+                inputStream.close();
+                mEditText.setText(builder.toString());
+            }
+        } catch (Throwable t) {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    // Метод для сохранения файла
+    private void saveFile(String fileName) {
+        try {
+            OutputStream outputStream = openFileOutput(fileName, 0);
+            OutputStreamWriter osw = new OutputStreamWriter(outputStream);
+            osw.write(mEditText.getText().toString());
+            osw.close();
+        } catch (Throwable t) {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + t.toString(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}
